66ef976234241ecb8a5cdf172e0a3b42bfab2b80
Lee Morris <lee@Lees-MacBook-Pro.local>
2018-03-18 19:53:53 +0000

    added check to ensure input is in range 1 to 3999 inclusive

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function convert(decimal) {
     if (typeof decimal !== 'number') return 'Please input a number';
+    if (decimal < 1 || decimal > 3999) return 'Please input a number between 1 and 3999 inclusive';
 }
 
 module.exports = { convert };
