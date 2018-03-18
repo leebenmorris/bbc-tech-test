682ea055ed37381a8decbb52531545750d545cb6
Lee Morris <lee@Lees-MacBook-Pro.local>
2018-03-18 19:47:07 +0000

    added tests to ensure only a number is accepted as input

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -6,4 +6,13 @@ describe('Roman Numeral Converter', function () {
     it('should be a function', function () {
         expect(convert).to.be.a('function');
     });
+
+    it('should only accept a number as input, returning an error message if not passed a number', function () {
+        const errorMsg = 'Please input a number';
+        expect(convert()).to.equal(errorMsg);
+        expect(convert({})).to.equal(errorMsg);
+        expect(convert([])).to.equal(errorMsg);
+        expect(convert(true)).to.equal(errorMsg);
+        expect(convert('1')).to.equal(errorMsg);
+    });
 });
