10e14d049ae74f526365f9b3a1b6748cde01c753
Lee Morris <lee@Lees-MacBook-Pro.local>
2018-03-18 20:46:56 +0000

    added recursive version of the conversion method

--- main.js
+++ main.js
@@ -33,4 +33,38 @@ function convert(input) {
     return output;
 }
 
-module.exports = { convert };
+function convert_recursive(input) {
+    // ensure input is a number
+    if (typeof input !== 'number') return 'Please input a number';
+
+    // ensure input is in the correct range
+    if (input < 1 || input > 3999) return 'Please input a number between 1 and 3999 inclusive';
+
+    // matched arrays of decimal number to roman numeral mappings
+    // with subtractive notation the numbers 4, 9, 40, 90, 400, 900 are special cases and so will be explicitly handled
+    const decimal = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+    const roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D', 'CM', 'M'];
+
+    // define recursive function for roman numeral conversion
+    function transform(input, output, index) {
+        // return result if input value is 0
+        if (input === 0) return output;
+
+        // if the input value minus the current decimal value is greater than or equal to zero
+        if (input - decimal[index] >= 0) {
+            // recursively call the transorm function, but subtract current decimal value, concatenate current roman numeral to output, but do not change the index
+            return transform(input - decimal[index], output + roman[index], index)
+        } else {
+            // recursively call the transform function, leaving input and output alone, but decrement the array index to select the next lowest number
+            return transform(input, output, index - 1)
+        }
+    }
+
+    // return result of recursive method after passing in inital values
+    return transform(input, '', decimal.length - 1);
+}
+
+module.exports = {
+    convert,
+    convert_recursive
+};
