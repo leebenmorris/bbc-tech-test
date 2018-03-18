dd1a9d29f2ec8a10613a1f5e3dda200a71a16793
Lee Morris <lee@Lees-MacBook-Pro.local>
2018-03-18 19:54:42 +0000

    added tests to ensure range is limited to between 1 and 3999 inclusive

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -15,4 +15,14 @@ describe('Roman Numeral Converter', function () {
         expect(convert(true)).to.equal(errorMsg);
         expect(convert('1')).to.equal(errorMsg);
     });
+
+    it('should only accept numbers in the range 1 to 3999 inclusive', function () {
+        const errorMsg = 'Please input a number between 1 and 3999 inclusive'
+        expect(convert(-2)).to.equal(errorMsg);
+        expect(convert(0)).to.equal(errorMsg);
+        expect(convert(4000)).to.equal(errorMsg);
+        expect(convert(1)).to.not.equal(errorMsg);
+        expect(convert(2000)).to.not.equal(errorMsg);
+        expect(convert(3999)).to.not.equal(errorMsg);
+    })
 });
